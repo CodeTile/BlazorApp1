2ad914ad10b2ec938a0e5cf1abc0908c472cdc55
CodeTile <CodeTile@dummy.com>
2025-09-24 16:30:12 +0100

    CoPilot changes

---
+++ BlazorApp1/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,26 @@
+﻿using BlazorApp1.Services;
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace BlazorApp1.Components.Layout
+{
+	public partial class MainLayout : LayoutComponentBase
+	{
+		[Inject] private NavigationManager NavigationManager { get; set; }
+		[Inject] private GlobalStateService GlobalState { get; set; }
+		protected override void OnInitialized()
+		{
+			var uri = NavigationManager.ToAbsoluteUri(NavigationManager.Uri);
+			var segments = uri.Segments;
+
+			if (segments.Length > 1)
+			{
+				var globalId = segments[1].TrimEnd('/');
+				GlobalState.GlobalId = globalId;
+			}
+
+			base.OnInitialized();
+		}
+	}
+}
--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -1,11 +1,12 @@
 using BlazorApp1.Components;
+using BlazorApp1.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
+builder.Services.AddSingleton<GlobalStateService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
